a82e74697d1d5d2999d0c583350f778e8d90de2d
MurilloWolf <mwolfcavalheiro@gmail.com>
2024-01-12 19:50:06 -0300

    feat: created a page to register products and clients

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,45 @@
 import { useState } from "react";
-import { Button } from "@/components/ui";
+import { Button, Collapsible } from "@/components/ui";
 import { useLocation } from "react-router-dom";
 import { CsvInput } from "@/components/functional";
+import { useStore } from "@/store/infra/zustand/store";
+import { useToast } from "@/components/ui";
+import { ArrowDown, ArrowUp } from "@phosphor-icons/react";
+
 export default function RegisterPage() {
   const location = useLocation();
+  const { toast } = useToast();
+
+  const [collapsibleCSV, setCollapsibleCSV] = useState(false);
+  const [collapsibleForm, setCollapsibleForm] = useState<boolean>(false);
+
   const [registerType, setRegisterType] = useState(
     location.state.type || "client"
   );
+  const { createClient, createProduct, hub } = useStore();
+
+  const handleRegisterClient = (client) => {
+    createClient(client);
+  };
+
+  const handleRegisterProduct = (product) => {
+    createProduct(product);
+  };
+
+  const handleOnSuccess = (data: any) => {
+    data.map((item) => {
+      if (registerType === "client") {
+        handleRegisterClient(item);
+        toast({
+          title: "Cadastro realizado",
+          description: "CSV carregado com successo",
+        });
+        return;
+      }
+      handleRegisterProduct(item);
+    });
+    console.log(data);
+  };
 
   const handleSelectType = (e) => {
     setRegisterType(e.target.name);
@@ -14,7 +47,8 @@ export default function RegisterPage() {
 
   return (
     <div>
-      <h1>Cadastrar</h1>
+      {console.info(hub)}
+      <h1>Cadastro</h1>
       <Button type="button" onClick={handleSelectType} name="client">
         Clientes
       </Button>
@@ -23,16 +57,52 @@ export default function RegisterPage() {
       </Button>
       {registerType === "client" ? (
         <div>
-          <h1>Registro de cliente</h1>
-          <h6>Cadastro por CSV</h6>
-          <CsvInput
-            onSuccess={() => console.log("sucesso")}
-            onError={() => console.log("erro")}
-            isValid={() => true}
-            parseData={(data) =>
-              data.map((item) => ({ name: item[0], email: item[1] }))
-            }
-          />
+          <h5>Registro de cliente</h5>
+          <Collapsible.Collapsible
+            className="w-full flex flex-col justify-center items-center"
+            open={collapsibleCSV}
+            onOpenChange={setCollapsibleCSV}
+          >
+            <Collapsible.CollapsibleTrigger asChild>
+              <Button variant="ghost" size="sm" className="my-6 w-full">
+                Cadastro por CSV
+                {collapsibleCSV ? (
+                  <ArrowUp size={18} className="mx-2" />
+                ) : (
+                  <ArrowDown size={18} className="mx-2" />
+                )}
+              </Button>
+            </Collapsible.CollapsibleTrigger>
+            <Collapsible.CollapsibleContent>
+              <CsvInput
+                onSuccess={handleOnSuccess}
+                onError={() => console.log("erro")}
+                isValid={() => true}
+                parseData={(data) =>
+                  data.map((item) => ({ name: item[0], email: item[1] }))
+                }
+              />
+            </Collapsible.CollapsibleContent>
+          </Collapsible.Collapsible>
+          <Collapsible.Collapsible
+            className="w-full flex flex-col justify-center items-center"
+            open={collapsibleForm}
+            onOpenChange={setCollapsibleForm}
+          >
+            <Collapsible.CollapsibleTrigger asChild>
+              <Button variant="ghost" size="sm" className="my-6 w-full">
+                Cadastro Manual
+                {collapsibleForm ? (
+                  <ArrowUp size={18} className="mx-2" />
+                ) : (
+                  <ArrowDown size={18} className="mx-2" />
+                )}
+              </Button>
+            </Collapsible.CollapsibleTrigger>
+            <Collapsible.CollapsibleContent>
+              FORMULARIO
+            </Collapsible.CollapsibleContent>
+          </Collapsible.Collapsible>
         </div>
       ) : (
         <div className="p-4">
@@ -40,7 +110,7 @@ export default function RegisterPage() {
           <div>
             <h6>Cadastro por CSV</h6>
             <CsvInput
-              onSuccess={() => console.log("sucesso")}
+              onSuccess={handleOnSuccess}
               onError={() => console.log("erro")}
               isValid={() => true}
               parseData={(data) =>
