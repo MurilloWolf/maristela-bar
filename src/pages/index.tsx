e70d4345bf86102e157e1257f54613f7898c2aa1
MurilloWolf <mwolfcavalheiro@gmail.com>
2024-01-13 10:51:35 -0300

    feat: create sellet page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,4 +2,5 @@ import Clients from "./Clients";
 import Home from "./Home";
 import Products from "./Products";
 import Register from "./Register";
-export { Clients, Home, Products, Register };
+import Seller from "./Seller";
+export { Clients, Home, Products, Register, Seller };
