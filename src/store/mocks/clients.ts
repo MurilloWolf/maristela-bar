0867082a9c68d03cb5f161771c771cf8128bbe80
MurilloWolf <mwolfcavalheiro@gmail.com>
2024-01-13 10:50:06 -0300

    feat: create fake data

--- src/store/mocks/clients.ts
+++ src/store/mocks/clients.ts
@@ -1,3 +1,26 @@
-const clients = [{ name: "murillo", id: "123", cpf: "122" }];
+const clients = [
+  {
+    name: "murillo",
+    id: "123",
+    cpf: "122",
+    wallet: 100,
+    email: "mwolf@gmail.com",
+  },
+  {
+    name: "Thais",
+    id: "234",
+    cpf: "234",
+    wallet: 1000,
+    email: "thais@gmail.com",
+  },
+  { name: "Luiz", id: "222", cpf: "222", wallet: 50, email: "luiz@gmail.com" },
+  {
+    name: "Thalita",
+    id: "333",
+    cpf: "333",
+    wallet: 60,
+    email: "thali@gmail.com",
+  },
+];
 
 export default clients;
---
+++ src/store/mocks/products.ts
@@ -0,0 +1,39 @@
+const products = [
+  {
+    name: "coca zero 350ml",
+    price: 3.5,
+    id: "111",
+    category: "bebidas",
+    quantity: 10,
+  },
+  {
+    name: "coca cola 350ml",
+    price: 3.5,
+    id: "222",
+    category: "bebidas",
+    quantity: 10,
+  },
+  {
+    name: "cone trufado",
+    price: 7.5,
+    id: "333",
+    category: "doces",
+    quantity: 100,
+  },
+  {
+    name: "sorvete",
+    price: 2,
+    id: "444",
+    category: "doces",
+    quantity: 500,
+  },
+  {
+    name: "sonho de valsa",
+    price: 2.5,
+    id: "555",
+    category: "doces",
+    quantity: 400,
+  },
+];
+
+export default products;
